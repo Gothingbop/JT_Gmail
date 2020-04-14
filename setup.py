18081dbd2ac740cf8e6b94cfd90437c480597a05
Jacob Thompson <Gothingbop@gmail.com>
2020-04-14 11:01:08 -0400

    Generalization
    - Made the workflow more general so multiple email addresses can be used.
    
    - Added functionality to retrieve emails

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements', 'r') as file:
 
 setuptools.setup(
     name="JT_Gmail",
-    version="0.0.1",
+    version="0.0.2",
     author="Jacob Thompson",
     author_email="Gothingbop@gmail.com",
     description="My Gmail API Wrapper",
